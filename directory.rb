e4795dfa392f1ec69a642768564be5dfe42b53c6
Andrew <akemp850@gmail.com>
2018-03-15 22:16:07 +0000

    added save function

--- directory.rb
+++ directory.rb
@@ -15,15 +15,18 @@ def input_students
     name = gets.chomp
   end
 end
+
 def print_header
 puts "The students of Villains Academy"
 puts "-------------"
 end
+
 def print_students_list
   @students.each_with_index do |student, index|
     puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
+
 def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
@@ -31,6 +34,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -46,6 +50,8 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
@@ -58,6 +64,16 @@ def interactive_menu
     print_menu
     process(gets.chomp)
   end
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 interactive_menu
