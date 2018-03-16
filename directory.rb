c136d9fc1b7c5832ef02c2f8cd50af802f06d09e
Andrew <akemp850@gmail.com>
2018-03-16 13:59:05 +0000

    Added autoload function

--- directory.rb
+++ directory.rb
@@ -3,17 +3,16 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  #create an empty array
-  #get the first name
-  name = gets.chomp
-  # while the name is not empty repeat this code
+  name = STDIN.gets.chomp
   while !name.empty? do
-    # add the student hash to the array
-    @students << {name: name, cohort: :november}
+    update_students_list({name: name, cohort: :november})
     puts "Now we have #{@students.count} students"
-    # get another name from the user
     name = gets.chomp
   end
+end
+
+def update_students_list(student_details)
+  @students << (student_details)
 end
 
 def print_header
@@ -65,7 +64,7 @@ end
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -79,13 +78,28 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
-    @students << {name: name, cohort: cohort.to_sym}
+    update_students_list({name: name, cohort: cohort.to_sym})
   end
   file.close
 end
 
+def try_load_students
+  filename = ARGV.first
+  if filename.nil?
+    load_students("students.csv")
+    puts "Loaded #{@students.count} from #{filename}"
+  elsif File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exist."
+    exit
+  end
+end
+
+try_load_students
 interactive_menu
