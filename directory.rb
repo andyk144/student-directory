ef276c8a2d74c5ac73dca998e25173f7273a2fba
Andrew <akemp850@gmail.com>
2018-03-16 10:41:50 +0000

    Added load file function

--- directory.rb
+++ directory.rb
@@ -15,15 +15,18 @@ def input_students
     name = gets.chomp
   end
 end
+
 def print_header
 puts "The students of Villains Academy"
 puts "-------------"
 end
+
 def print_students_list
   @students.each_with_index do |student, index|
     puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
+
 def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
@@ -32,6 +35,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -49,6 +53,8 @@ def process(selection)
       show_students
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
       exit
     else
@@ -69,6 +75,15 @@ def save_students
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
     file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
 end
